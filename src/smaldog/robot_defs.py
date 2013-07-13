202ad6a5faf615a35b131b6e0513414ea752c413
Mike Ferguson <mike@vanadiumlabs.com>
2013-07-13 13:52:02 -0700

    update with numbers from CAD

--- src/smaldog/robot_defs.py
+++ src/smaldog/robot_defs.py
@@ -26,14 +26,21 @@ class SMALdog:
                 "rr_pitch_joint", "lr_pitch_joint",
                 "rr_flex_joint", "lr_flex_joint",
                 "rr_knee_joint", "lr_knee_joint"]
+    # Determined on real robot:
     mins =     [152, 450,   1,  336, 193, 160, 450, 231,  323,   1, 159, 192]
     maxs =     [575, 868, 689, 1023, 857, 824, 813, 575, 1023, 683, 819, 855]
-    neutrals = [512, 512, 442,  582, 302, 722, 512, 512,  582, 442, 722, 302]
+    # From CAD:
+    #  - Shoulder flex is 20.400209 degrees off the 0 when the line between shoulder
+    #    flex and knee flex is upright. 20.400209/300 * 1024 = 69.63
+    #  - Knee flex is 30.177936 degrees off the 0 when the line between the knee
+    #    flex and the foot is upright. 30.177936/300 * 1024 = 204.19
+    neutrals = [512, 512, 442,  582, 308, 716, 512, 512,  582, 442, 716, 308]
+    # Determined on real robot:
     signs =    [ -1,   1,  -1,    1,   1,  -1,   1,  -1,    1,  -1,  -1,   1]
 
     X_SHOULDER = 0.086  # Meters between front and back legs /2
     Y_SHOULDER = 0.019  # Meters between front/back legs /2
 
     L_SHOULDER = 0.050  # Meters distance from shoulder pitch servo to shoulder flex servo
-    L_FEMUR = 0.064     # Meters distance from flex servo to knee servo
-    L_TIBIA = 0.086     # Meters distance from knee servo to foot
+    L_FEMUR = 0.065     # Meters distance from flex servo to knee servo (from CAD)
+    L_TIBIA = 0.088     # Meters distance from knee servo to foot (from CAD)
