978e86c921852b8dcce350dddda5f62a49fd7fb4
Michael Ferguson <fergs@unboundedrobotics.com>
2014-02-08 01:51:33 -0800

    change stand up pose, so that legs don't collide

--- scripts/stand_up.py
+++ scripts/stand_up.py
@@ -10,16 +10,6 @@ from trajectory_msgs.msg import *
 from smaldog.robot_defs import *
 
 robot = SMALdog()
-stance = [[robot.X_SHOULDER+0.01, -0.04, -0.08],   # Right Front
-          [-robot.X_SHOULDER+.06, 0.06, -0.08],   # Left Rear
-          [robot.X_SHOULDER+.01, 0.06, -0.08],    # Left Front
-          [-robot.X_SHOULDER+.01, -0.06, -0.08]]  # Right Rear
-
-stance = [[robot.X_SHOULDER-.06, -0.07, -0.04],   # Right Front
-          [-robot.X_SHOULDER+.06, 0.07, -0.04],   # Left Rear
-          [robot.X_SHOULDER-.06, 0.07, -0.04],    # Left Front
-          [-robot.X_SHOULDER+.06, -0.07, -0.04]]  # Right Rear
-
 rospy.init_node("stand")
 
 rospy.loginfo('Connecting to body_controller...')
@@ -27,20 +17,29 @@ client = actionlib.SimpleActionClient('body_controller/follow_joint_trajectory',
 client.wait_for_server()
 rospy.loginfo('Connected.')
 
+# Move to starting position
 goal = FollowJointTrajectoryGoal()
 goal.trajectory.joint_names = robot.names
 
 goal.trajectory.points.append(JointTrajectoryPoint())
-stance = [[robot.X_SHOULDER-.06, -0.07, -0.04],   # Right Front
-          [-robot.X_SHOULDER+.06, 0.07, -0.04],   # Left Rear
-          [robot.X_SHOULDER-.06, 0.07, -0.04],    # Left Front
-          [-robot.X_SHOULDER+.06, -0.07, -0.04]]  # Right Rear
+stance = [[robot.X_SHOULDER, -0.07, -0.04],   # Right Front
+          [-robot.X_SHOULDER, 0.07, -0.04],   # Left Rear
+          [robot.X_SHOULDER, 0.07, -0.04],    # Left Front
+          [-robot.X_SHOULDER, -0.07, -0.04]]  # Right Rear
 pose = robot.getIK(stance)
 goal.trajectory.points[0].positions = [pose[name] for name in robot.names]
-goal.trajectory.points[0].time_from_start = rospy.Duration(0.0)
+goal.trajectory.points[0].time_from_start = rospy.Duration(1.0)
 print [pose[name] for name in robot.names]
 
-for i in range(4):
+goal.trajectory.header.stamp = rospy.Time.now()
+client.send_goal(goal)
+client.wait_for_result()
+
+# Already here
+goal.trajectory.points[0].time_from_start = rospy.Duration(0.0)
+
+# Now actually stand up
+for i in range(6):
     goal.trajectory.points.append(JointTrajectoryPoint())
     stance[0][2] -= 0.01
     stance[1][2] -= 0.01
@@ -48,7 +47,7 @@ for i in range(4):
     stance[3][2] -= 0.01
     pose = robot.getIK(stance)
     goal.trajectory.points[-1].positions = [pose[name] for name in robot.names]
-    goal.trajectory.points[-1].time_from_start = goal.trajectory.points[-2].time_from_start + rospy.Duration(0.5)
+    goal.trajectory.points[-1].time_from_start = goal.trajectory.points[-2].time_from_start + rospy.Duration(0.1)
     #print [pose[name] for name in robot.names]
 
 goal.trajectory.header.stamp = rospy.Time.now()
